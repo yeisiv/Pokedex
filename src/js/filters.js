6da48a69a1a0576e3cbabf182fe004374f3e5a6f
yeisiv <jcarlos.ivillalta@gmail.com>
2026-08-28 01:58:40 +0200

    feat: Implementar chips de filtros activos con opción de quitar individualmente

--- src/js/filters.js
+++ src/js/filters.js
@@ -24,27 +24,8 @@ function cerrarPaneles() {
 }
 
 export function initFiltros(listaPoke, tiposPoke, tema) {
-    const filtroActivo = {region: null, tipos: [], busqueda: ""};
+    const filtroActivo = { region: null, tipos: [], busqueda: "" };
     let pokemonsFiltrados = [];
-
-    function aplicarfiltros(){
-        pokemonsFiltrados= listaPoke.filter(pokemon =>{
-            let pasaRegion = true;
-            let pasaTipo = true;
-            let pasaBusqueda = true;
-            if(filtroActivo.region != null){
-                pasaRegion = pokemon.region === filtroActivo.region;
-            }
-            if(filtroActivo.tipos.length > 0){
-                pasaTipo = filtroActivo.tipos.every(tipo => pokemon.tipos.includes(tipo));
-            }
-            if(filtroActivo.busqueda.trim() != ""){
-                pasaBusqueda = pokemon.nombre.toLowerCase().startsWith(searchInput.value.toLowerCase()) || String(pokemon.numero).startsWith((searchInput.value));
-            }
-            return pasaBusqueda && pasaRegion && pasaTipo;
-        })
-        renderGrid(pokemonsFiltrados, tiposPoke, tema);
-    }
 
     botonesControl.forEach(boton => {
         const id = boton.getAttribute('aria-controls');
@@ -55,7 +36,7 @@ export function initFiltros(listaPoke, tiposPoke, tema) {
     document.addEventListener('click', (event) => {
         const dentroControles = event.target.closest('.controls');
         const dentroPaneles = event.target.closest('.paneles');
-        
+
         if (!dentroControles && !dentroPaneles) {
             cerrarPaneles();
         }
@@ -73,6 +54,7 @@ export function initFiltros(listaPoke, tiposPoke, tema) {
         }
 
         aplicarfiltros();
+        renderChips();
     });
 
     btnClean.addEventListener('click', () => {
@@ -82,27 +64,101 @@ export function initFiltros(listaPoke, tiposPoke, tema) {
     });
 
     const filtroTipos = document.querySelector('.tipo-opciones');
-    
+
     filtroTipos.addEventListener('change', (event) => {
         const seleccionados = Array.from(filtroTipos.querySelectorAll('input:checked'));
-        
+        console.log(seleccionados);
         if (seleccionados.length > 2) {
             event.target.checked = false;
-        }
+        } else {
+            filtroActivo.tipos = (seleccionados.map(input => ponerMayusculaPrimeraLetra(input.value)));
 
-        filtroActivo.tipos = (seleccionados.map(input => ponerMayusculaPrimeraLetra(input.value)));
+            aplicarfiltros();
+            renderChips();
+        }
         console.log(filtroActivo);
 
-        aplicarfiltros();
     });
 
     const regiones = document.querySelector('.region-opciones');
 
-    regiones.addEventListener('change', (event) =>{
+    regiones.addEventListener('change', (event) => {
         const seleccionado = regiones.querySelector('input[name="region"]:checked');
 
         filtroActivo.region = ponerMayusculaPrimeraLetra(seleccionado.value);
 
         aplicarfiltros();
+        renderChips();
     });
+
+    function aplicarfiltros() {
+        pokemonsFiltrados = listaPoke.filter(pokemon => {
+            let pasaRegion = true;
+            let pasaTipo = true;
+            let pasaBusqueda = true;
+            if (filtroActivo.region != null) {
+                pasaRegion = pokemon.region === filtroActivo.region;
+            }
+            if (filtroActivo.tipos.length > 0) {
+                pasaTipo = filtroActivo.tipos.every(tipo => pokemon.tipos.includes(tipo));
+            }
+            if (filtroActivo.busqueda.trim() != "") {
+                pasaBusqueda = pokemon.nombre.toLowerCase().startsWith(searchInput.value.toLowerCase()) || String(pokemon.numero).startsWith((searchInput.value));
+            }
+            return pasaBusqueda && pasaRegion && pasaTipo;
+        })
+        renderGrid(pokemonsFiltrados, tiposPoke, tema);
+    }
+
+    function renderChips() {
+        const filtrosActivos = document.querySelector(".filtros-activos");
+        filtrosActivos.innerHTML = "";
+        const template = document.getElementById("filtros-template");
+        if (filtroActivo.region !== null) {
+            const clon = template.content.cloneNode(true);
+            clon.querySelector(".chip-texto").textContent = "Región: " + filtroActivo.region;
+
+            const boton = clon.querySelector('button');
+            boton.setAttribute('aria-label', "Quitar filtro de región " + filtroActivo.region);
+            boton.addEventListener('click', () => {
+                regiones.querySelector('input[value="' + filtroActivo.region.toLowerCase() + '"]').checked = false;
+                filtroActivo.region = null;
+                aplicarfiltros();
+                renderChips();
+            });
+            filtrosActivos.appendChild(clon);
+        }
+        if (filtroActivo.tipos.length > 0) {
+            filtroActivo.tipos.forEach(tipo => {
+                const clon = template.content.cloneNode(true);
+                clon.querySelector(".chip-texto").textContent = "Tipo: " + tipo;
+
+                const boton = clon.querySelector('button');
+                boton.setAttribute('aria-label', "Quitar filtro de tipo " + tipo);
+                boton.addEventListener('click', () => {
+                    filtroTipos.querySelector('input[value="' + tipo.toLowerCase() + '"]').checked = false;
+                    filtroActivo.tipos = filtroActivo.tipos.filter(t => t !== tipo);
+                    aplicarfiltros();
+                    renderChips();
+                    
+                });
+                filtrosActivos.appendChild(clon);
+            });
+        }
+        if (filtroActivo.busqueda.trim() != "") {
+            const clon = template.content.cloneNode(true);
+            clon.querySelector(".chip-texto").textContent = "Búsqueda: " + filtroActivo.busqueda;
+
+            const boton = clon.querySelector('button');
+            boton.setAttribute('aria-label', "Quitar filtro de búsqueda por: " + filtroActivo.region);
+            boton.addEventListener('click', () => {
+                searchInput.value = "";
+                filtroActivo.busqueda = "";
+                btnClean.hidden = true;
+                aplicarfiltros();
+                renderChips();
+            });
+            filtrosActivos.appendChild(clon);
+        }
+    }
 }
